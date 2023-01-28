f3bdc59ad7b62b22615f245c348c4d10645bf649
Anna199421 <111239829+Anna199421@users.noreply.github.com>
2023-01-28 16:05:33 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,35 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const lightboxRef = document.querySelector(".gallery");
-const lightboxMarkup = createBoxItem(galleryItems);
-lightboxRef.insertAdjacentHTML(`afterbegin`, lightboxMarkup);
+const galleryContainer = document.querySelector(".gallery");
 
-function createBoxItem(galleryItems) {
-    return galleryItems
-        .map(
-            ({ preview, original, description }) =>
-                `<a class="gallery__item" href="${original}">
-                <img
-                class="gallery__image"
-                src="${preview}"
-                alt="${description}"
-                />
-            </a>`
-        )
-        .join("");
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup(galleryItems));
+
+function galleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>`
+    )
+    .join("");
 }
 
-lightboxRef.addEventListener("click", onBoxRefClick);
-
-function onBoxRefClick(event) {
-    event.preventDefault();
-    if (!event.target.classList.contains("gallery__image")) {
-        return;
-    }
-    const lightbox = new SimpleLightbox(".gallery a", {
-        captionsData: "alt",
-        captionDelay: 250,
-    });
-}
-console.log(galleryItems);
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
